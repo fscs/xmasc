da6f8ce23e73c10af8f17beaffb1ccc2af2cc0cf
Tobias Witt <tobias.witt@hhu.de>
2014-11-26 16:31:02 +0100

    Add basic auth

--- app.rb
+++ app.rb
@@ -4,6 +4,10 @@ require './config/environments'
 
 require './app/models/imp'
 
+use Rack::Auth::Basic, "Restricted Area" do |username, password|
+  username == 'wichtel' and password == 'wichtelmeister'
+end
+
 get '/' do
   File.read 'public/dist/index.html'
 end
