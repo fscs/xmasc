9c25e25288489ef886b9917a73985e571852563b
Tobias Witt <tobias.witt@hhu.de>
2014-11-26 01:41:48 +0100

    Don't validate presence of calendar but number of rest türchen

--- app/models/imp.rb
+++ app/models/imp.rb
@@ -1,4 +1,11 @@
 class Imp < ActiveRecord::Base
-  validates :name, :email, :calendar, presence: { message: "ist erforderlich" }
+  validates :name, :email, presence: { message: "ist erforderlich" }
   validates :email, uniqueness: { message: "ist schon angemeldet" }
+  validate :rest_of_calendar
+
+  def rest_of_calendar
+    if self.class.where(calendar: calendar).length >= 24
+      errors.add :calendar, "ist schon ausgebucht!"
+    end
+  end
 end
