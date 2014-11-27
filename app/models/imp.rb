8092552041728a1a699223b7f48dc621afe2abdc
Tobias Witt <tobias.witt@hhu.de>
2014-11-27 23:09:28 +0100

    Validate total amount of Türchen

--- app/models/imp.rb
+++ app/models/imp.rb
@@ -1,11 +1,15 @@
 class Imp < ActiveRecord::Base
   validates :name, :email, presence: { message: "ist erforderlich" }
   validates :email, uniqueness: { message: "ist schon angemeldet" }
-  validate :rest_of_calendar
+  validate :rest_of_calendar, on: :create
 
   def rest_of_calendar
     if self.class.where(calendar: calendar).length >= 24
       errors.add :calendar, "ist schon ausgebucht!"
+    end
+
+    if self.count >= 48
+      errors.add :calendar, "in beiden Kalendern sind alle Türchen vergeben"
     end
   end
 end
