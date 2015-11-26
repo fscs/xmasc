69ce1a22c92e96813a3cf2da89ca12fc94bb6e47
Tobias Witt <tobias.witt@hhu.de>
2015-11-26 11:14:01 +0100

    Use sqlite3 in production

--- config/environments.rb
+++ config/environments.rb
@@ -20,17 +20,6 @@ configure :development do
 end
 
 configure :production do
-  db = URI.parse(ENV['DATABASE_URL'] || 'postgres://localhost/mydb')
-
-  ActiveRecord::Base.establish_connection(
-    :adapter => db.scheme == 'postgres' ? 'postgresql' : db.scheme,
-    :host     => db.host,
-    :username => db.user,
-    :password => db.password,
-    :database => db.path[1..-1],
-    :encoding => 'utf8'
-  )
-
   ActionMailer::Base.delivery_method = :sendmail
 end
 
