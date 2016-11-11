9f6867cf70766103716d58b983a7e181cbb5471f
Tobias Witt <tobias.witt@hhu.de>
2016-11-11 18:31:24 +0100

    Improver tests for roller

--- spec/lib/roller_spec.rb
+++ spec/lib/roller_spec.rb
@@ -5,15 +5,21 @@ describe Roller do
   before do
     72.times do
       imp = build :random_imp
-      until imp.valid?
-        imp.calendar = ["Lego Star Wars", "Lego City", nil].sample
-      end
+      imp.calendar = ["Lego Star Wars", "Lego Hotwheels", "Lego Barbie", nil].sample
       imp.save
     end
   end
 
   it "rolls a random distribution of tuerchen for all imps" do
-    described_class.roll
+    begin
+      old_stdout = $stdout
+      $stdout = StringIO.new("", "w")
+      described_class.roll
+      output = $stdout.string
+    ensure
+      $stdout = old_stdout
+    end
+
     imp_calendars = Imp.all.group_by { |imp| imp.calendar }
 
     map = Proc.new { |imp| imp.tuerchen }
@@ -27,5 +33,7 @@ describe Roller do
     expect(barbie).to match_array (1..24).to_a
     expect(imp_calendars[nil]).to be_nil
 
+    puts
+    puts output.truncate 100
   end
 end
